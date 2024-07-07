6c76b0f2611741f8b45b57bd9bef2ac1107afc3c
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2024-07-08 00:14:37 +0600

    successfully ran a hello world script, great start!

--- observer.py
+++ observer.py
@@ -1,14 +1,16 @@
 import subprocess
 
-from utils.os_helppers import get_bash_command
+from utils.os_helppers import get_bash_script_command_with_repo, get_bash_script_only_command
 
 # The observer will poll the repository periodically
 def poll():
     repo_src = './'
-    while True:
+    command_to_poll = get_bash_script_only_command('./scripts/update_repo.sh')
+    output = False
+    while not output:
         # check for changes in the git repo
         # If changes are found latest commit id is written to a file
-        command_to_run = get_bash_command('./', './')
+        output = subprocess.check_output(command_to_poll)
+        print(output)
 
-command_to_run = get_bash_command('./', './')
-print(command_to_run)
+poll()
