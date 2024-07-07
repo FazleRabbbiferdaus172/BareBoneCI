d57bb60cfe0f3d44ed28c7d63b03616d045fa548
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2024-07-08 00:13:45 +0600

    utils changes, Although not need because of wsl??git status

--- utils/os_helppers.py
+++ utils/os_helppers.py
@@ -5,15 +5,16 @@ import subprocess
 def get_os_type():
     return platform.system()
 
-def get_bash_command(script_path, repo_path):
+def get_bash_script_only_command(script_path):
     os_type = get_os_type()
     script_path = os.path.normpath(script_path)
-    repo_path = os.path.normpath(repo_path)
-
-    if os_type == 'Windows':
-        return ["bash", script_path, repo_path]
-    elif os_type in ['Linux', 'Darwin']: 
-        return [script_path, repo_path]
+    if os_type in ['Windows' ,'Linux', 'Darwin']: 
+        return ["bash", script_path]
     else:
         raise OSError(f"Unsupported operating system: {os_type}")
+
+def get_bash_script_command_with_repo(script_path, repo_path):
+    repo_path = os.path.normpath(repo_path)
+    script_command = get_bash_script_only_command(script_path)
+    return script_command + [repo_path]
 
