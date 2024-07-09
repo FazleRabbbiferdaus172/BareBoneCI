8e877bf24ca5983e8a3e5909d0608aa70c307e63
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2024-07-10 00:59:38 +0600

    now can change directory to repo_location and reset repo to head for fetching current commit

--- observer.py
+++ observer.py
@@ -4,13 +4,13 @@ from utils.os_helppers import get_bash_script_command_with_repo, get_bash_script
 
 # The observer will poll the repository periodically
 def poll():
-    repo_src = './'
-    command_to_poll = get_bash_script_only_command('./update_repo.sh')
+    repo_src = '../test_repo_clone_obs'
+    command_to_poll = get_bash_script_command_with_repo('./update_repo.sh', repo_path=repo_src)
     output = False
     while not output:
         # check for changes in the git repo
         # If changes are found latest commit id is written to a file
         output = subprocess.run(command_to_poll, cwd='./scripts', capture_output=True, text=True)
-        print(output.stdout, '\n', output.stderr)
+        print("From Python", output.stdout, '\n', output.stderr)
 
 poll()
--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -2,4 +2,8 @@
 
 source ./run_or_fail.sh
 
-run_or_fail "I failed" echo "Hello, world"
+run_or_fail "I failed" echo $1
+
+run_or_fail "Repo Location not found" pushd $1 1> /dev/null # 1> /dev/null removes the output of pushd
+
+run_or_fail "Git reset failed" git reset --hard HEAD
