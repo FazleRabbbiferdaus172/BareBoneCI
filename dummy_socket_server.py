0a5ea5e2e7eac08ac47c07b6c3df83054f793daf
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2024-07-29 02:31:58 +0600

    a bad implementation of forkserver generator :hankey:

--- dummy_socket_server.py
+++ dummy_socket_server.py
@@ -1,61 +1,106 @@
 import socket
 import time
 import os
-import sys
 
-server_host = 'localhost'
-server_port = 8888
+# server_host = "localhost"
+# server_port = 8888
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # s.bind((server_host, server_port))
-# s.listen()
-# this should be inside a loop? but how do i determine that request has ended?
 
-with s:
-     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-     s.bind((server_host, server_port))
-     s.listen()
-     while True:
-          (clientsocket_connection, address) = s.accept()
-          with clientsocket_connection:
-               pid = os.fork()
-               if pid == 0:
-                    request_msg = clientsocket_connection.recv(1024).decode('utf-8')
-                    print("From: {}:{} {} || by process {}".format(*address,request_msg, os.getpid()))
-                    time.sleep(20)
-                    clientsocket_connection.sendall('pong by child pid {}'.format(os.getpid()).encode('utf-8'))
-                    # clientsocket_connection.close()
-                    break
-
-# try:
+# with s:
+#     s.listen()
 #     while True:
-#             (clientsocket_connection, address) = s.accept()
-#             # print("accepted by process {}".format(os.getpid()))
-#             pid = os.fork() # this pid is the pid of child process
+#         (clientsocket_connection, address) = s.accept()
+#         with clientsocket_connection:
+#             parent_pid = os.getpid()
+#             pid = os.fork()
 #             if pid == 0:
-#                 request_msg = clientsocket_connection.recv(1024).decode('utf-8')
-#                 print("From: {}:{} {} || by process {}".format(*address,request_msg, os.getpid()))
+#                 request_msg = clientsocket_connection.recv(
+#                     1024).decode("utf-8")
+#                 print(
+#                     "From: {}:{} {} || by process {} || accepeted by process {}".format(
+#                         *address, request_msg, os.getpid(), parent_pid
+#                     )
+#                 )
 #                 time.sleep(20)
-#                 clientsocket_connection.sendall('pong by child pid {}'.format(os.getpid()).encode('utf-8'))
-#                 clientsocket_connection.close()
+#                 clientsocket_connection.sendall(
+#                     "pong by child pid {} and parent pid".format(
+#                         os.getpid(), parent_pid
+#                     ).encode("utf-8")
+#                 )
+#                 # clientsocket_connection.close()
 #                 break
-# except Exception as e:
-#     s.close()
-#     sys.exit(0)
-    # print("Looped by {}".format(os.getpid()))
-    # print()
-    # print()
 
+server_pid = os.getpid()
+pid = os.getpid()
+class ForkServer:
 
-# while True:
-#     (clientsocket_connection, address) = s.accept()
-#     request_msg = clientsocket_connection.recv(1024).decode('utf-8')
-#     print("From: {}:{} {} || by process {}".format(*address,request_msg, os.getpid()))
-#     time.sleep(20)
-#     clientsocket_connection.sendall('pong by child pid {}'.format(os.getpid()).encode('utf-8'))
-#     clientsocket_connection.close()
+    def __init__(self, server_host, server_port):
+        self.server_host = server_host
+        self.server_port = int(server_port)
+        print(__name__)
+        self.server_socket = self._initialize_server()
+        server_pid = os.fork()
+        if server_pid == 0:
+            self._start_serving()
 
-# if pid > 0:
-# print("Closed by process {}".format(pid))
-# s.close()
+    def _initialize_server(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((self.server_host, self.server_port))
+        return server_socket
+
+    def _start_serving(self):
+        with self.server_socket:
+            self.server_socket.listen()
+            while True:
+                (clientsocket_connection, address) = self.server_socket.accept()
+                with clientsocket_connection:
+                        pid = os.fork()
+                        if pid == 0:
+                            request_msg = self._receive_request(
+                                                        clientsocket_connection)
+                            response_msg = self.process_request(request_msg)
+                            self._send_response(
+                                                        clientsocket_connection, response_msg)
+                            break
+
+    @staticmethod
+    def _receive_request(clientsocket_connection):
+        request_msg = ""
+        while True:
+            request_msg_chunk = clientsocket_connection.recv(
+                1024).decode("utf-8")
+            if not request_msg_chunk:
+                break
+            request_msg += request_msg_chunk
+            if '\n' in request_msg:
+                break
+        return request_msg.strip()
+
+    def process_request(self, request_msg):
+        response_msg = "pong"
+        return response_msg
+
+    @staticmethod
+    def _send_response(clientsocket_connection, response_msg):
+        clientsocket_connection.sendall(
+            response_msg.encode("utf-8")
+        )
+
+# if __name__ == '__main__':
+#     print("main process not import")
+#     if server_pid > 0 and pid > 0:
+#         new_server = ForkServer(
+#             server_host = "localhost", server_port = 8888
+#         )
+#         print(server_pid, pid)
+#         new_server_2 = ForkServer(
+#             server_host = "localhost", server_port = 8889
+#         )
+#         print(server_pid, pid)
+#         new_server_3 = ForkServer(
+#             server_host = "localhost", server_port = 8890
+#         )
--- observer.py
+++ observer.py
@@ -3,18 +3,20 @@ import socket
 import os
 
 from utils.os_helppers import get_bash_script_command_with_repo, get_bash_script_only_command
+from dummy_socket_server import ForkServer
 
 
 repo_src = '../test_repo_clone_obs'
 command_to_poll = get_bash_script_command_with_repo('./update_repo.sh', repo_path=repo_src)
 
 dispathcer_server_host = 'localhost'
-dispatcher_server_port = 8888
+# dispatcher_server_port = 8888
 
-def reach_dispathcer():
+def reach_dispathcer(dispatcher_server_port):
+    print(dispatcher_server_port)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((dispathcer_server_host, dispatcher_server_port))
-    client_socket.send('ping'.encode('utf-8'))
+    client_socket.send('ping\n'.encode('utf-8'))
     dispathcer_server_response = client_socket.recv(1024)
     print(dispathcer_server_response.decode('utf-8'))
     client_socket.close()
@@ -35,8 +37,18 @@ while 1 == 0:
             commit_id = f.readline()
             reach_dispathcer()
     
+new_server = ForkServer(
+            server_host = "localhost", server_port = 8888
+        )
+new_server_2 = ForkServer(
+            server_host = "localhost", server_port = 8889
+        )
+new_server_3 = ForkServer(
+            server_host = "localhost", server_port = 8890
+        )
 
-
-reach_dispathcer()
+# reach_dispathcer(8888)
+# reach_dispathcer(8889)
+# reach_dispathcer(8890)
 
 
