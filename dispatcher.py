41f95783421f1b0ad984b9b585497fc96d55e286
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2024-08-03 04:23:17 +0600

    modifications on forkserver for dispatcher server, more changes to come in the dispatcher server and te handler

--- dispatcher.py
+++ dispatcher.py
@@ -5,6 +5,25 @@ from dummy_socket_server import ForkServer
 dispatcher_host = 'localhost'
 dispatcher_port = 8888
 
+class DispatcherForkServer(ForkServer):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.pending_commits = []
+        self.dispatched_commits = {}
+
+    def add_to_pending_commits(self, commit_id):
+        self.pending_commits.append(commit_id)
+
+    def remove_from_pending_commits(self, commit_id):
+        self.pending_commits.remove(commit_id)
+
+    def add_to_dispatched_commits(self, commit_id, runner):
+        self.dispatched_commits[commit_id] = runner
+
+    def remove_from_dispatch_commits(self, commit_id):
+        del self.dispatched_commits[commit_id]
+
 def request_handler(server, request_msg):
     response_msg = 'Request Invalid'
     if request_msg == 'hi':
